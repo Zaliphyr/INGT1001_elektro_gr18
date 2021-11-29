a55b85ed8c71955a264decb6d1c4b7cab14ebd1d
Lizardion1 <lizardion1@gmail.com>
2021-11-29 11:28:54 +0100

    Changed F.PI

--- Minnepæl 2/F-PI.py
+++ Minnepæl 2/F-PI.py
@@ -1,10 +1,33 @@
 # Hei og velkommen til F-Pi main
 
-main()
+r = (0, 0, 0) # road
+g = (0, 255, 0) # grass
+
+
+example_map = [ [g, r, r, r, r, r, r, g],
+                [g, r, r, r, r, r, r, g],
+                [g, r, r, r, r, r, r, g],
+                [g, r, r, r, r, r, r, g],
+                [g, r, r, r, r, r, r, g],
+                [g, r, r, r, r, r, r, g],
+                [g, r, r, r, r, r, r, g],
+                [g, r, r, r, r, r, r, g],
+                [g, r, r, r, r, r, r, g],
+                [g, r, r, r, r, r, r, g],
+                [g, r, r, r, r, r, r, g],
+                [g, r, r, r, r, r, r, g],
+                [g, r, r, r, r, r, r, g],
+                [g, r, r, r, r, r, r, g],
+                [g, r, r, r, r, r, r, g],
+                [g, r, r, r, r, r, r, g]]
+
+
+
+def main():
+    pass
 
 
 
 
-
-
-if __name__ == "__main__"
+if __name__ == "__main__":
+    main()

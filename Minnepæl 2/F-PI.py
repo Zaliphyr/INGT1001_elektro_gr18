32a3d7c0ccb321266cfababae141fbb221662284
AstorNaut42 <danielastor@live.no>
2021-11-29 23:09:25 +0100

    Oppdatert coin_placer()
    Mye greier, vettafaen ka som skjer lenger

--- Minnepæl 2/F-PI.py
+++ Minnepæl 2/F-PI.py
@@ -1,6 +1,8 @@
 # Hei og velkommen til F-Pi main
 
+
 import random
+
 r = (0, 0, 0) # road / black
 g = (0, 255, 0) # grass / green
 o = (255, 0, 0) # obstacle
@@ -43,23 +45,23 @@ def map_creator():                  # Function to create an empty map
 
 
 
-
-def coin_placer():
-    g_map = map_creator()                         # Defining g_map variable from the function 
+def coin_placer():                      # Function that places a coin somwhere on the map
+    g_map = map_creator()
 
     obstacle = True
-    while obstacle == True:
-        x = int(random.randint(1, 6))             # x is a random integer which corresponds to a pixel on the top row
-        y = int(random.randint(0, 3))             # y is a random integer which defines how often a coin should be placed
-        coin_placement = g_map[0][x]              # Variable for which pixel to place the coin. Adds to first row 
+    while obstacle == True:             # The code will run as long as theres an obstacle where the coin is proposed to go        
+        x = int(random.randint(1, 6))   # x is a random integer which corresponds to a pixel on the top row
+        y = int(random.randint(0, 3))   # y is a random integer which defines how often a coin should be placed
+        #print("y: ", y)
+        coin_placement = g_map[0][x]    # Variable for which pixel to place the coin. Adds to first row 
 
-        if o == coin_placement:                   # Checks to see if theres an obstacle in where the coin should be placed
+        if o == coin_placement:         # Checks to see if theres an obstacle in where the coin should be placed
             obstacle = True
             #print("Oi! Her er det en hindring!")
-        elif y == 1:                              # There's a 25% chance that y is 1, and then a coin will be placed
-            g_map[0][x] = c                       # Replaces the the current index with a coin
-            obstacle = False                      # Breaks the loop since theres no obstacle
-            
+        elif y == 1:                    # There's a 25% chance that y is 1, and then a coin will be placed
+            g_map[0][x] = c             # Replaces the current index with a coin, and stops the while loop
+            obstacle = False
+
             """
             print("Nytt kart:")
             for row in g_map:
@@ -68,6 +70,7 @@ def coin_placer():
         else:
             #print("Ingen coin denne gangen.")
             break
+    
     return g_map
 
 
@@ -84,18 +87,12 @@ def mov_map(map):                   # Function to move the map
     return p_map                    # Returns the new map
 
 
-
-
-
 def main():
-
-    coin_placer()
-
     pass
+
 
 
 
 if __name__ == "__main__":
     main()
-
 

d56cd9b22502a8c2dc18d282e6b3d6f6edca0063
Lizardion1 <lizardion1@gmail.com>
2021-11-29 12:10:44 +0100

    Fjernet kommentar

--- Minnepæl 2/F-PI.py
+++ Minnepæl 2/F-PI.py
@@ -1,7 +1,7 @@
 # Hei og velkommen til F-Pi main
 
 r = (0, 0, 0) # road / black
-g = (0, 255, 0) # grass / green SKRIVEFEIL FUCK OFF
+g = (0, 255, 0) # grass / green
 
 
 example_map = [ [g, r, r, r, r, r, r, g],

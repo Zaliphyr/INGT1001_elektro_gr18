742a4c461c5105f75105f7f9c08f7983b705d97e
markor01 <94351248+markor01@users.noreply.github.com>
2021-11-30 09:07:46 +0100

    Bil posisjon
    Lagde to funksjoner, én for gyroskop og én for joystick

--- Minnepæl 2/F-PI.py
+++ Minnepæl 2/F-PI.py
@@ -25,7 +25,26 @@ example_map = [ [g, r, r, r, r, r, r, g],
                 [g, r, r, r, r, r, r, g]]
 
 
-bil_pos = 0
+def car_pos_joy(prev_pos): # Function for the position of the car controlled by the joystick,
+                           # with previous position as input shown by an integer between 0 and 7
+    from sense_hat import SenseHat # Importing SenseHat from sense_hat library
+    sense = SenseHat()
+    event = sense.stick.wait_for_event() # Henter data fra joystick
+    joy_input = event[1]
+
+    if joy_input == "down":
+        position = prev_pos - 1
+    elif joy_input == "up":
+        position = prev_pos + 1
+    else:
+        position = prev_pos
+
+    if position < 0: # The car can't go further to the left than 0, therefor if the position is negative:
+        position = 0 # Set the position to 0
+    elif position > 7: # The car can't go further to the right than 7, therefor if the position is over 7:
+        position = 7 # Set the position to 7
+    
+    return position
 
 
 

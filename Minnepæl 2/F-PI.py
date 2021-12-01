3156ab63ec0827eaf1f3e28b22abf0fb9beb3af6
AstorNaut42 <danielastor@live.no>
2021-12-01 15:03:49 +0100

    Update F-PI.py

--- Minnepæl 2/F-PI.py
+++ Minnepæl 2/F-PI.py
@@ -189,13 +189,13 @@ def coin_placer(g_map):                      # Function that places a coin somwh
         x = int(random.randint(1, 6))   # x is a random integer which corresponds to a pixel on the top row
         y = int(random.randint(0, 3))   # y is a random integer which defines how often a coin should be placed
         #print("y: ", y)
-        coin_placement = g_map[0][x]    # Variable for which pixel to place the coin. Adds to first row 
+        coin_placement = g_map[3][x]    # Variable for which pixel to place the coin. Adds to first row 
 
         if o == coin_placement:         # Checks to see if theres an obstacle in where the coin should be placed
             obstacle = True
             #print("Oi! Her er det en hindring!")
         elif y == 1:                    # There's a 25% chance that y is 1, and then a coin will be placed
-            g_map[0][x] = c             # Replaces the current index with a coin, and stops the while loop
+            g_map[3][x] = c             # Replaces the current index with a coin, and stops the while loop
             obstacle = False
 
             """
@@ -225,42 +225,25 @@ def mov_map(map):                   # Function to move the map
 def obstacle(kart, score):          # Function to create obstacle in first row
     if score < 1000:                # Checking how high the score is
         obst = random.randint(1, 6) # Random number
-        kart[0][obst] = o           # Changes the elements in the first row
+        kart[3][obst] = o           # Changes the elements in the first row
     elif 1000 <= score < 2000:
         for i in range(2):
             obst = random.randint(1, 6)
-            kart[0][obst] = o
+            kart[3][obst] = o
     elif 2000 <= score < 3000:
         for i in range(3):
             obst = random.randint(1, 6)
-            kart[0][obst] = o
+            kart[3][obst] = o
     elif 3000 <= score < 4000:
         for i in range(4):
             obst = random.randint(1, 6)
-            kart[0][obst] = o
+            kart[3][obst] = o
     else:
         for i in range(5):
             obst = random.randint(1, 6)
-            kart[0][obst] = o
+            kart[3][obst] = o
     
-
     return kart, obst
-
-
-def enable_screen(game_map, car_pos) :               # Function that sets pixels on sens hat
-    road_screen = game_map.copy()
-    road_screen = road_screen[8:]     # Chooses the eight last lists of the list
-    
-    screen_pixels = []
-    for e in road_screen :
-        for pixel in e :
-            screen_pixels.append(pixel)   # Adds all elements in from main map to screen_pixels (total of 64 elements)
-
-    vehicle_pixel = 56 + car_pos      # Finds the last 8 elements, where the car will move horizontal
-    screen_pixels[vehicle_pixel] = v  # Set the vehicle to life with posistion from vehicle function
-
-    sense.set_pixels(screen_pixels)
-
 
 
 def enable_third_person(game_map, car_pos) :
@@ -276,17 +259,31 @@ def enable_third_person(game_map, car_pos) :
     road_screen = road_screen[12:]     # Chooses the four last lists of the list
 
 
-    incoming_obstacles = 0 
-    obst = obstacle(incoming_obstacle)
-    incoming_obstacles = incoming_obstacles[0][obst]
-    
     screen_pixels = []
-    for e in road_screen :
-        for pixel in e :
-            screen_pixels.append(pixel)   # Adds all elements in from main map to screen_pixels (total of 64 elements)
 
-    vehicle_pixel = 56 + car_pos      # Finds the last 8 elements, where the car will move horizontal
-    screen_pixels[vehicle_pixel] = v  # Set the vehicle to life with posistion from vehicle function
+    for row in sunset_picture :
+        screen_pixels.append(row)         # Adds all elements from sunset_picture. Total of 32 elements
+
+    print(len(screen_pixels))
+
+    print(road_screen)
+    for row in road_screen:
+        screen_pixels.append(row)
+    print(len(screen_pixels))
+
+    """
+    obstacle_pos2 = screen_pixels[2][obst]
+    obstacle_pos1 = screen_pixels[1][obst]
+    obstacle_pos0 = screen_pixels[0][obst]
+    red_pixel = (255, 0, 0)
+
+    screen_pixels.insert(o, obstacle_pos2) 
+    screen_pixels.insert(red_pixel, obstacle_pos1)
+    screen_pixels.insert(red_pixel, obstacle_pos0)     
+    """
+
+    vehicle_pixel = 56 + car_pos                # Finds the last 8 elements, where the car will move horizontal
+    screen_pixels[vehicle_pixel] = v            # Set the vehicle to life with posistion from vehicle function
 
     sense.set_pixels(screen_pixels)
 
@@ -355,7 +352,8 @@ def run_game():
     while running:
         for i in range(3):
             vehicle_pos = car_pos_joy(vehicle_pos)
-            enable_screen(game_map, vehicle_pos)
+            #enable_screen(game_map, vehicle_pos)
+            enable_third_person(game_map, vehicle_pos)
             point, collision = move_collision(game_map, vehicle_pos)
             if collision:
                 running = False
